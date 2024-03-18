f467e638c050b640bc83d065f5690e19cb0a7889
Gabriel Schmidt Cordeiro <gabrielscordeiro2012@gmail.com>
2024-03-18 20:40:17 -0300

    style: 💄 Add Favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,8 +18,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
-            <body className={inter.variable}>{children}</body>
+        <html lang="en" className={inter.variable}>
+            <body>{children}</body>
         </html>
     )
 }
