a3c5af1c25179c52e0b4996a0fdd2aef8cfeea87
Gabriel Schmidt Cordeiro <gabrielscordeiro2012@gmail.com>
2024-03-19 20:16:34 -0300

    feat: 🎸 Creating application layout

---
+++ src/app/(store)/layout.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from 'react'
+
+import { Header } from '@/components/header'
+
+export default function StoreLayout({ children }: { children: ReactNode }) {
+    return (
+        <div>
+            <Header />
+            {children}
+        </div>
+    )
+}
