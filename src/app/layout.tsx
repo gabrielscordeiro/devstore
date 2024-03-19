52b5dc1072413b9fdfe971335624a1f0e3dc1885
Gabriel Schmidt Cordeiro <gabrielscordeiro2012@gmail.com>
2024-03-19 20:48:25 -0300

    feat: 🎸 Header component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en" className={inter.variable}>
-            <body>{children}</body>
+            <body className="bg-zinc-950 text-zinc-50 antialiased">{children}</body>
         </html>
     )
 }
