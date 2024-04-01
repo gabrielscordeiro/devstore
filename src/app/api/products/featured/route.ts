a9adcd7c852a334c9114bf8c4693d9581989b5f6
Gabriel Schmidt Cordeiro <gabrielscordeiro2012@gmail.com>
2024-04-01 19:57:13 -0300

    feat: 🎸 Loading screen on home

--- src/app/api/products/featured/route.ts
+++ src/app/api/products/featured/route.ts
@@ -1,5 +1,6 @@
 import data from '../data.json'
 export async function GET() {
+    await new Promise((resolve) => setTimeout(resolve, 1000))
     const featuredProduts = data.products.filter((product) => product.featured)
 
     return Response.json(featuredProduts)
