5eeb6198ce818b165b4f491330c55c092d06973d
Gabriel Schmidt Cordeiro <gabrielscordeiro2012@gmail.com>
2024-04-02 20:29:48 -0300

    feat: 🎸 Product details page - Fetching data from API

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -1,12 +1,35 @@
 import Image from 'next/image'
 
-export default function ProductPage() {
+import { api } from '@/data/api'
+import { Product } from '@/data/types/product'
+
+interface ProductProps {
+    params: {
+        slug: string
+    }
+}
+async function getProduct(slug: string): Promise<Product> {
+    const response =  await api(`/products/${slug}`, {
+        next: {
+            revalidate: 60 * 60 // 1 hour
+        }
+    })
+
+    const products = await response.json()
+
+    return products
+}
+
+export default async function ProductPage({ params }: ProductProps) {
+    const product = await getProduct(params.slug)
+
+
     return (
         <div className="relative grid max-h-[860px] grid-cols-3">
             <div className="col-span-2 overflow-hidden">
                 <Image
-                    src="/moletom-never-stop-learning.png"
-                    alt=""
+                    src={product.image}
+                    alt={product.title}
                     width={1000}
                     height={1000}
                     quality={100}
@@ -15,15 +38,27 @@ export default function ProductPage() {
 
             <div className="flex flex-col justify-center px-12">
                 <h1 className="leading-tightd text-3xl font-bold">
-                    Moletom never stop learning
+                    {product.title}
                 </h1>
                 <p className="mt-2 leading-relaxed text-zinc-400">
-                    Moletom fabricado com 88% de algodão e 12% de poliéster
+                    {product.description}
                 </p>
 
                 <div className="mt-8 flex items-center gap-3">
-                    <span className="inline-block rounded-full bg-violet-500 px-5 py-2.5 font-semibold">$129</span>
-                    <span className="text-sm text-zinc-400">Em 12x s/ juros de $10,75</span>
+                    <span className="inline-block rounded-full bg-violet-500 px-5 py-2.5 font-semibold">
+                        {product.price.toLocaleString('en-US', {
+                            style: 'currency',
+                            currency: 'USD',
+                            minimumFractionDigits: 0,
+                            maximumFractionDigits: 0
+                        })}
+                    </span>
+                    <span className="text-sm text-zinc-400">
+                       12x of {(product.price / 12 ).toLocaleString('en-US', {
+                            style: 'currency',
+                            currency: 'USD',
+                        })}
+                    </span>
                 </div>
 
                 <div className="mt-8 space-y-4">
--- src/app/api/products/featured/route.ts
+++ src/app/api/products/featured/route.ts
@@ -1,4 +1,5 @@
 import data from '../data.json'
+
 export async function GET() {
     await new Promise((resolve) => setTimeout(resolve, 1000))
     const featuredProduts = data.products.filter((product) => product.featured)
