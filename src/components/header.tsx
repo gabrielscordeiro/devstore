79feb01a71535f25c3737a172fa89520a2090386
Gabriel Schmidt Cordeiro <gabrielscordeiro2012@gmail.com>
2024-04-08 20:31:07 -0300

    feat: 🎸 Search form component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,8 @@
-import { Search } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 
 import { CartWidget } from '@/components/cart-widget'
+import { SearchForm } from '@/components/search-form'
 
 export function Header() {
 
@@ -13,10 +13,7 @@ export function Header() {
                     devstore
                 </Link>
 
-                <form className="flex w-[320px] items-center gap-3 rounded-full bg-zinc-900 px-5 py-3 ring-zinc-700">
-                    <Search className="size-5 text-zinc-500" />
-                    <input placeholder="Search products..." className="flex-1 bg-transparent text-sm outline-none placeholder:text-zinc-500" />
-                </form>
+                <SearchForm />
             </div>
 
 
