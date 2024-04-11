4dcd8a0b7446aa85bac014da6c6e4e91f9e772b6
Gabriel Schmidt Cordeiro <gabrielscordeiro2012@gmail.com>
2024-04-11 20:49:38 -0300

    fix: 🐛 Add fallback to suspense component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -14,7 +14,7 @@ export function Header() {
                     devstore
                 </Link>
 
-                <Suspense>
+                <Suspense fallback={null}>
                     <SearchForm />
                 </Suspense>
             </div>
